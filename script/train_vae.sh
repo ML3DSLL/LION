cb12b2b8c7a1ca6b27bc48c305bcf3f305c62fc4
Joong-Won Seo <joong-won.seo@tum.de>
2023-07-01 13:37:10 +0200

    downscale to work on RTX3060

--- script/train_vae.sh
+++ script/train_vae.sh
@@ -6,14 +6,14 @@ fi
 DATA=" ddpm.input_dim 3 data.cates car "
 NGPU=$1 # 
 num_node=1
-BS=32 
+BS=8
 total_bs=$(( $NGPU * $BS ))
 if (( $total_bs > 128 )); then 
     echo "[WARNING] total batch_size larger than 128 may lead to unstable training, please reduce the size"
     exit
 fi
 
-ENT="python train_dist.py --num_process_per_node $NGPU "
+ENT="python train_dist.py --num_process_per_node $NGPU --resume --pretrained=../exp/0627/car/464ad6h_hvae_lion_B8/checkpoints/snapshot"
 kl=0.5  
 lr=1e-3
 latent=1
@@ -32,8 +32,8 @@ $ENT ddpm.num_steps 1 ddpm.ema 0 \
     data.num_workers 4 \
     ddpm.loss_weight_emd 1.0 \
     trainer.epochs 8000 data.random_subsample 1 \
-    viz.viz_freq -400 viz.log_freq -1 viz.val_freq 200 \
-    data.batch_size $BS viz.save_freq 2000 \
+    viz.viz_freq -400 viz.log_freq -1 viz.val_freq 20 \
+    data.batch_size $BS viz.save_freq 20 \
     trainer.type 'trainers.hvae_trainer' \
     model_config default shapelatent.model 'models.vae_adain' \
     shapelatent.decoder_type 'models.latent_points_ada.LatentPointDecPVC' \
@@ -45,4 +45,4 @@ $ENT ddpm.num_steps 1 ddpm.ema 0 \
     shapelatent.decoder_num_points 2048 \
     data.tr_max_sample_points 2048 data.te_max_sample_points 2048 \
     ddpm.loss_type $loss cmt "lion" \
-    $DATA viz.viz_order [2,0,1] data.recenter_per_shape False data.normalize_global True 
+    $DATA viz.viz_order [2,0,1] data.recenter_per_shape False data.normalize_global True
